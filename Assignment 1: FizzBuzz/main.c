efc07b1ae439c8b67db49a35974e5561825d1f56
nekomeo <elleti@Elles-MacBook-Air.local>
2017-04-25 22:18:07 -0700

    first commit

--- Assignment 1: FizzBuzz/main.c
+++ Assignment 1: FizzBuzz/main.c
@@ -9,7 +9,25 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    for (int n = 1; n <= 100; n++)
+    {
+        if (n % 3 == 0 && n % 5 == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+        else if (n % 3 == 0)
+        {
+            printf("Fizz\n");
+        }
+        else if (n % 5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        else
+        {
+            printf("%d\n", n);
+        }
+    }
+    
     return 0;
 }
